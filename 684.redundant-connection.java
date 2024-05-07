ea846cfadea73073e3dff2d9be84c384114d1a89
Paul Chuang <paul.d.chuang@gmail.com>
2024-05-07 13:39:35 +0800

    Using Java array instead of Map is a lot faster

--- 684.redundant-connection.java
+++ 684.redundant-connection.java
@@ -5,15 +5,12 @@
  */
 
 // @lc code=start
-import java.util.HashMap;
-import java.util.Map;
 class Solution {
-    public int[] findRedundantConnection(int[][] edges) {
-        Map<Integer, Integer> m = new HashMap<Integer, Integer>();
-        Map<Integer, Integer> rank = new HashMap<Integer, Integer>();
-        for (int i = 1; i <= edges.length; i++) {
-            m.put(i, -1);
-            rank.put(i, 0);
+    public int[] findRedundantConnection(int[][] edges) {        
+        int[] m = new int[edges.length];    
+        int[] rank = new int[edges.length];
+        for (int i = 0; i < edges.length; i++) {            
+            m[i] = -1;            
         }
         for (int i = 0; i < edges.length; i++){
             boolean res = union(edges[i], m, rank);
@@ -21,23 +18,23 @@ class Solution {
         }
         return edges[0];
     }
-    private int find(int x, Map<Integer, Integer> m) {
-        if (m.get(x) == -1) {
+    private int find(int x, int[] m) {
+        if (m[x] == -1) {
             return x;
         }
-        m.put(x, this.find(m.get(x), m));
-        return m.get(x);
+        m[x] = this.find(m[x], m);
+        return m[x];
     }
-    private boolean union(int[] edge, Map<Integer, Integer> m, Map<Integer, Integer> rank) {
-        int x = edge[0]; int y = edge[1];
+    private boolean union(int[] edge, int[] m, int[] rank) {
+        int x = edge[0]-1; int y = edge[1]-1;
         int x_p = this.find(x, m); int y_p = this.find(y, m);
         if (x_p != y_p){
-            if (rank.get(x_p) < rank.get(y_p)) {
-                m.put(x_p, y_p);
-                rank.put(y_p, rank.get(y_p) + 1);
+            if (rank[x_p] < rank[y_p]) {
+                m[x_p] = y_p;
+                rank[y_p] = rank[y_p] + 1;
             } else {
-                m.put(y_p, x_p);
-                rank.put(x_p, rank.get(x_p) + 1);
+                m[y_p] = x_p;
+                rank[x_p] = rank[x_p] + 1;
             }
             return false;
         }
